92cbb16520071ac35969705b9aad3432fc0c5297
Laukhin Vadim <laukhin.va@phystech.edu>
2022-10-05 01:08:09 +0300

    Задачи по ЦАПу

---
+++ 4-3-pwm.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GP
+
+#Задай номер пина который хочешь настроить на ШИМ
+pin=24
+GP.setmode(GP.BCM)
+GP.setup(pin,GP.OUT)
+try:
+    while TRUE:
+        print("Для остановки программы введите: stop")
+        dc=input("Введи коэфициент заполнения: ")
+        if dc == "stop":
+            break
+        if dc.isdigit():
+            v = 100/int(dc) * 3.3
+            print(v, "B")
+            a=GP.PWM(pin,dc)
+            a.start(dc)
+            input("Нажми enter чтобы остановить ШИМ")
+            a.stop()
+        else:
+            print("Вводите число от 0 до 100, а не символы!")
+    
+finally:
+    GP.output(pin,0)
+    GP.cleanup()
+
